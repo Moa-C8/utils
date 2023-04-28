4521777d14e34dc68a1ef2b04073822061ee6ac5
C8 <creser08.wtf@gmail.com>
2023-04-28 01:07:33 +0100

    Add files via upload

---
+++ deleteRuns.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+savesFolder = "./saves/"
+liste = os.listdir(savesFolder)
+i = 0
+for folder in liste:
+    if "Random Speedrun #" in folder:
+        i += 1
+        shutil.rmtree(savesFolder+folder)
+
+print(i)
